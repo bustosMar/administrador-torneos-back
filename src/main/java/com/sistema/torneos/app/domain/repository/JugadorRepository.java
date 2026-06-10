8d1aa6e46b357416ac70d928456d00e2f02a5991
bustosMar <marco_bustos9@hotmail.com>
2026-06-10 17:01:12 -0600

    Ajustes

--- src/main/java/com/sistema/torneos/app/domain/repository/JugadorRepository.java
+++ src/main/java/com/sistema/torneos/app/domain/repository/JugadorRepository.java
@@ -2,11 +2,11 @@ package com.sistema.torneos.app.domain.repository;
 
 import com.sistema.torneos.app.domain.entity.Jugador;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
 	
-	Jugador findByNombreAndApellidoAndFechaNacimiento(String nombre, String apellido, Date fechaNacimiento);
+	Jugador findByNombreAndApellidoAndFechaNacimiento(String nombre, String apellido, LocalDate fechaNacimiento);
 }
--- src/main/java/com/sistema/torneos/app/web/model/JugadorModel.java
+++ src/main/java/com/sistema/torneos/app/web/model/JugadorModel.java
@@ -1,8 +1,8 @@
 package com.sistema.torneos.app.web.model;
 
-import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
 
 import lombok.*;
 
@@ -14,7 +14,7 @@ public class JugadorModel {
     private Long id;
     private String nombre;
     private String apellido;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    private Date fechaNacimiento;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate fechaNacimiento;
     
 }
